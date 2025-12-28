8fd0206d0cfee21e27393b21f84ea3083eba44f9
Tyler Corum <22278328+TEC10101@users.noreply.github.com>
2025-12-28 02:42:05 -0600

    Version bump to 3.08.01

--- ArkInventory.lua
+++ ArkInventory.lua
@@ -27,8 +27,8 @@ ArkInventory.Const = { -- constants
 
 	Program = {
 		Name = "ArkInventory",
-		Version = 3.0800,
-		UIVersion = "3.08.00",
+		Version = 3.0801,
+		UIVersion = "3.08.01",
 		--Beta = "Beta xx-xx",
 	},
 
