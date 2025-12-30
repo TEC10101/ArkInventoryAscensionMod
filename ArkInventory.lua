9112aac4f228483615b82e9981048b283b206bf4
Tyler Corum <22278328+TEC10101@users.noreply.github.com>
2025-12-30 16:49:34 -0600

    3.08.05

--- ArkInventory.lua
+++ ArkInventory.lua
@@ -27,8 +27,8 @@ ArkInventory.Const = { -- constants
 
 	Program = {
 		Name = "ArkInventory",
-		Version = 3.0804,
-		UIVersion = "3.08.04",
+		Version = 3.0805,
+		UIVersion = "3.08.05",
 		--Beta = "Beta xx-xx",
 	},
 
